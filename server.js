dc13c818224aaeaeb792b4573dc15606ce07a18a
Anoupz <anoupz@gmail.com>
2015-03-11 00:31:40 -0700

    Allow X-Access_token as header paramter

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'X-Access-Token,Content-Type, Authorization, Content-Length, X-Requested-With');
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
-        res.send(200);
+        res.sendStatus(200);
     }
     else {
         next();
